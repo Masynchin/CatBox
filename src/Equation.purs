0517095a2e7a403f457f8c85b307464731d214e1
Max Smirnov <masynchin@gmail.com>
2022-07-17 22:53:36 +0300

    Add parser (#3)

--- src/Equation.purs
+++ src/Equation.purs
@@ -13,3 +13,8 @@ data Equation = Cat | Box (NonEmptyList Equation)
 instance showEquation :: Show Equation where
   show Cat = "😼"
   show (Box equations) = "[" <> foldMap show equations <> "]"
+
+instance eqEquation :: Eq Equation where
+  eq Cat Cat = true
+  eq (Box eqs1) (Box eqs2) = eq eqs1 eqs2
+  eq _ _ = false
---
+++ src/Parser.purs
@@ -0,0 +1,29 @@
+module Parser
+  ( equation
+  )
+  where
+
+import Prelude
+
+import CatBox (Equation(..))
+import Control.Alt ((<|>))
+import Parsing (Parser)
+import Parsing.Combinators (many1)
+import Parsing.String (char, string)
+
+type P a = Parser String a
+
+cat :: P Equation
+cat = do
+  _ <- string "😼"
+  pure Cat
+
+box :: P Equation
+box = do
+  _ <- char '['
+  equations <- many1 (box <|> cat)
+  _ <- char ']'
+  pure (Box equations)
+
+equation :: P Equation
+equation = box <|> cat
