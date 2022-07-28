d6e7bd9d14d35f98eabe8bdcdba60d7b3ba85bc4
Max Smirnov <masynchin@gmail.com>
2022-07-29 00:36:55 +0300

    Keep select in model

--- spago.dhall
+++ spago.dhall
@@ -8,9 +8,12 @@
   , "flame"
   , "foldable-traversable"
   , "lists"
+  , "maybe"
   , "parsing"
   , "prelude"
   , "test-unit"
+  , "tuples"
+  , "web-events"
   ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
--- src/Main.purs
+++ src/Main.purs
@@ -5,32 +5,39 @@ import Prelude
 import CatBox (Equation(..))
 import Data.Array (fromFoldable)
 import Data.Either (Either(..))
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Flame (QuerySelector(..), Html)
 import Flame.Application.NoEffects as FAN
+import Flame.Event (onSelect)
 import Flame.Html.Attribute as HA
 import Flame.Html.Element as HE
 import Parser (equation)
 import Parsing (runParser)
 
-type Model = { input :: String }
+type Model = { input :: String
+             , select :: Tuple Int Int
+             }
 
 init :: Model
-init = { input: "" }
+init = { input: ""
+       , select: Tuple (-1) (-1)
+       }
 
-data Message = Input String
+data Message = Input String | Select Int Int
 
 update :: Model -> Message -> Model
 update model =
   case _ of
     Input newInput -> model { input = newInput }
+    Select start end -> model { select = Tuple start end }
 
 view :: Model -> Html Message
 view model = HE.main "main" [renderInput, renderEquation model.input]
 
 renderInput :: Html Message
 renderInput = HE.label_ [ HE.text "Input Equation:"
-                        , HE.input [HA.type' "text", HA.onInput Input]
+                        , HE.input [HA.type' "text", HA.onInput Input, onSelect Select]
                         ]
 
 renderEquation :: String -> Html Message
